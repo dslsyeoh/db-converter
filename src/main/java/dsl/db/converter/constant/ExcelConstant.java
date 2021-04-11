8d242b73c3606104e8888a8d7177c57c88f1e765
dslsyeoh <syeoh91x@gmail.com>
2021-04-11 16:15:30 +0800

    add ExcelConstant

---
+++ src/main/java/dsl/db/converter/constant/ExcelConstant.java
@@ -0,0 +1,14 @@
+/*
+ * Author Steven Yeoh
+ * Copyright (c) 2021. All rights reserved.
+ */
+
+package dsl.db.converter.constant;
+
+public interface ExcelConstant
+{
+    String START_COLUMN = "START";
+    int COLUMN_NANE_ROW_GAP = 1;
+    int DATA_TYPE_ROW_GAP = 2;
+    int DATA_ROW_GAP = 3;
+}
