5bf9ec2ef5e795d7dca844d3ef17f80459a2ba87
dslsyeoh <syeoh91x@gmail.com>
2021-04-19 23:19:56 +0800

    fix typo

--- src/main/java/dsl/db/converter/constant/ExcelConstant.java
+++ src/main/java/dsl/db/converter/constant/ExcelConstant.java
@@ -8,7 +8,7 @@ package dsl.db.converter.constant;
 public interface ExcelConstant
 {
     String START_COLUMN = "START";
-    int COLUMN_NANE_ROW_GAP = 1;
+    int COLUMN_NAME_ROW_GAP = 1;
     int DATA_TYPE_ROW_GAP = 2;
     int DATA_ROW_GAP = 3;
 }
